d92024c9ce6831174c9e37bf86e34954b378d1ae
Akash-29E <parmaraakash45@gmail.com>
2025-10-10 10:26:36 -0400

    Changed color palette

--- src/SideDrawer.jsx
+++ src/SideDrawer.jsx
@@ -7,7 +7,7 @@ export default function SideDrawer() {
         <div className="drawer-img-container">
           <div className="profile-img-ripple">
             <img
-              src="/profile2.jpg"
+              src="./profile2.jpg"
               alt="Profile"
               className="drawer-profile-img"
             />
@@ -16,10 +16,10 @@ export default function SideDrawer() {
           <div className="drawer-profile-designation">Web Developer</div>
             <div className="drawer-profile-socials">
               <a href="https://www.linkedin.com/in/parmar-akash/" target="_blank" rel="noopener noreferrer" className="drawer-social-icon" aria-label="LinkedIn">
-                <img src="/linkedin.svg" alt="LinkedIn" width="24" height="24" style={{ display: 'block' }} />
+                <img src="./linkedin.svg" alt="LinkedIn" width="24" height="24" style={{ display: 'block' }} />
               </a>
               <a href="https://github.com/Akash-29E/" target="_blank" rel="noopener noreferrer" className="drawer-social-icon" aria-label="GitHub">
-                <img src="/github.svg" alt="GitHub" width="24" height="24" style={{ display: 'block' }} />
+                <img src="./github.svg" alt="GitHub" width="24" height="24" style={{ display: 'block' }} />
               </a>
             </div>
           <a href="/JohnDoeCV.pdf" download className="drawer-download-btn">Download CV</a>
